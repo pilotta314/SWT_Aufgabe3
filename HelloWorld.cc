65f28717cc9680b6b13b1bad3dac6c3c8c4f206b
Janek Prange <Janek.Prange@posteo.de>
2021-01-10 20:25:40 +0100

    Grüßt zurück Mars und Jupiter

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -6,5 +6,6 @@ int main() {
     std::cout << "Hello World!";
     std::cout << "Hello Mars!";
     std::cout << "Hello Jupiter!";
+    std::cout << "Hello from Mars and Jupiter!";
     return 0;
 }
