f028f7509a789b9b2dab35d74b4e129e923ff125
Janek Prange <Janek.Prange@posteo.de>
2021-01-10 20:53:13 +0100

    Grußwechsel

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -3,9 +3,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
     std::cout << "Hello Mars!";
-    std::cout << "Hello Jupiter!";
     std::cout << "Hello from Mars and Jupiter!";
+    std::cout << "By World!";
+    std::cout << "By Jupiter!";
     return 0;
 }
