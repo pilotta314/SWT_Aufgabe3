5803e4a16257e3f47ce70aca8aea1e8b4030ebcd
stina.schaefer <stina.schaefer@web.de>
2021-01-10 21:02:47 +0100

    coole Grüße

--- HelloWorld.cc
+++ HelloWorld.cc
@@ -4,8 +4,8 @@
 
 int main() {
     std::cout << "Hello World!";
-    std::cout << "Hello Mars!";
-    std::cout << "Hello Jupiter!";
+    std::cout << "Bye Mars!";
+    std::cout << "Heloolo Jupiter!";
     std::cout << "Hello from Mars and Jupiter!";
     return 0;
 }
